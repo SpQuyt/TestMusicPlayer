3db9ad575b9c7864fd28d768a1a84d45fe69f2cb
spquyt <spquyt@gmail.com>
2020-05-20 16:41:44 +0700

    fix: add seekbar + rotate image

--- app/src/main/java/com/example/testmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/testmusicplayer/MainActivity.java
@@ -2,22 +2,37 @@ package com.example.testmusicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView tvTitle;
-    private Button btnPrevious;
-    private Button btnPlay;
-    private Button btnNext;
+    private TextView tvTitle, textTimeCurrent, textTimeTotal;
+    private Button btnPrevious, btnNext, btnPlay;
+    private SeekBar seekBar;
     private MediaPlayer mediaPlayer;
     private ArrayList<Song> arrSong = new ArrayList<Song>();
+    private final Handler timer = new Handler();
     private int position = 0;
+    private Runnable updatePerSecond = new Runnable() {
+        @Override
+        public void run() {
+            int length = mediaPlayer.getCurrentPosition();
+            seekBar.setProgress(length);
+            textTimeCurrent.setText(intToTimeString(length));
+            timer.postDelayed(this, 1000);
+        }
+    };
+    private ObjectAnimator rotateAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.rotate);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +66,31 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer = MediaPlayer.create(MainActivity.this, arrSong.get(position).getFile());
             }
         });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser && mediaPlayer != null) {
+                    mediaPlayer.seekTo(progress);
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                        rotateAnimator.pause();
+                        resumeSong();
+                    }
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+
+    private String intToTimeString(int ms) {
+        SimpleDateFormat formatter = new SimpleDateFormat("mm:ss");
+        return formatter.format(ms);
     }
 
     private void findId() {
@@ -58,6 +98,9 @@ public class MainActivity extends AppCompatActivity {
         btnPrevious = findViewById(R.id.btnPrevious);
         btnPlay = findViewById(R.id.btnPlay);
         btnNext = findViewById(R.id.btnNext);
+        seekBar = findViewById(R.id.seekBar);
+        textTimeCurrent = findViewById(R.id.textTimeCurrent);
+        textTimeTotal = findViewById(R.id.textTimeTotal);
     }
 
     private void addSong(){
@@ -65,5 +108,17 @@ public class MainActivity extends AppCompatActivity {
         arrSong.add(new Song("BuongDoiTayNhauRa", R.raw.buongdoitaynhaura));
         arrSong.add(new Song("KhongPhaiDangVuaDau", R.raw.khongphaidangvuadau));
         mediaPlayer = MediaPlayer.create(MainActivity.this, arrSong.get(position).getFile());
+    }
+
+    private void resumeSong() {
+        int length = mediaPlayer.getCurrentPosition();
+        mediaPlayer.seekTo(length);
+        mediaPlayer.start();
+        rotateAnimator.resume();
+        btnPlay.setBackgroundResource(android.R.drawable.ic_media_pause);
+        tvTitle.setText(arrSong.get(position).getTitle());
+        seekBar.setMax(mediaPlayer.getDuration());
+        seekBar.setProgress(length);
+        timer.postDelayed(updatePerSecond, 1000);
     }
 }
